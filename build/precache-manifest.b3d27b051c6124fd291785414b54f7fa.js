7b1c5db17294fe3b76195437c61f36ca196274ee
Ka Vang <Ka.Vang@outlook.com>
2020-02-29 00:28:30 -0600

    created new repository

---
+++ build/precache-manifest.b3d27b051c6124fd291785414b54f7fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "520be3a912e60bea21243fba28182da1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71d0d3783f91c897fd7b",
+    "url": "/static/css/main.7af9c0d2.chunk.css"
+  },
+  {
+    "revision": "6e8f29f659723a048183",
+    "url": "/static/js/2.a6fe207e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a6fe207e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71d0d3783f91c897fd7b",
+    "url": "/static/js/main.f477ffa0.chunk.js"
+  },
+  {
+    "revision": "8dc3100d013e4e88cf59",
+    "url": "/static/js/runtime-main.960f005f.js"
+  },
+  {
+    "revision": "c2ed6dcd23a18b1340ea719a94ddd28c",
+    "url": "/static/media/directory.c2ed6dcd.jpg"
+  }
+]);
